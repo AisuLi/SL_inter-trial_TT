d8f63b66f08a15a15c77f26220a3e6a95579599e
AisuLi <45638074+AisuLi@users.noreply.github.com>
2019-12-29 12:11:36 +0100

    Add files via upload

---
+++ exp1_noDis/new_behAnaly_exp1.m
@@ -0,0 +1,210 @@
+function behAnaly_SL(directory)
+
+
+if ~exist('directory','var') || isempty(directory) 
+	if ~ispc
+		directory = '';
+	else
+		directory = 'H:\Desktop\expData\SL_horVer_20181008\Exp1_20181028\Data\AnalyData\exp1_noDis';
+	end
+end
+
+
+try
+
+cd(directory);
+
+randLocGroupNum = 1; %% 1 = merge[nonPair+pairNonReg]
+analyBy         = 1;
+regularGroup    = 2; %% 2 = random+regular;
+allfiles        = dir(fullfile(pwd,'*.mat'));
+allSubData      = [];
+allSubAnaly     = [];
+allSubDataInfo  = {'subGrp';'subNo';'sessionNo';'blockNo';'targLoc';'targShape';
+				   'targColor';'targLine';'regular';'regularPairedAcc';'acc';'rt';'slowLimit';'fastLimit';
+				   'distance';'targetShape';'targetColor';'hemisphere';'preTtrialCorrect'}; % trial-to-trial relationship
+
+for iMat = 1:size(allfiles,1)
+	cFile = allfiles(iMat).name;
+	load(cFile);
+	cSubData  	= [];
+	cSubAnaly 	= []; 
+	BlockNum  	= size(designMatrix,3);
+
+	cPair = [4 2; 8 6];
+	if ~mod(str2num(subInfo{1}),2) 
+		cPair([1 2],:) = cPair([2 1],:);
+	end
+
+
+	for iBlock = 1:BlockNum
+		cBlockData   = [repmat(iBlock,size(designMatrix,1),1) squeeze(designMatrix(:,[1:end-1],iBlock))];
+		cSubData     = [cSubData;cBlockData];
+		cBlockAnaly  = [];
+
+
+		%%%%%%%%%%%%%%%%%%  calculate trial-by-trial relationship  %%%%%%%%%%%%%%%%%%%%%
+	    
+	    %% --------  column 1 = distance ---------/
+		for iTrial = 1:size(cBlockData,1)
+
+			if iTrial == 1 % first trial no preTrial
+				
+				cBlockAnaly(iTrial,1) = 100; 
+
+			else  % nonFirst Trial
+				
+				cDis = abs(diff(cBlockData([iTrial-1,iTrial],2)));
+
+				if cDis<=4
+					cBlockAnaly(iTrial,1) = cDis;
+				else 
+					cBlockAnaly(iTrial,1) = 8-cDis;
+				end
+			end
+		end
+		%%% ----------- distance end --------------\
+
+
+		%% ----- column 2/3 = target shape/color repeat(1) or switch(0) -------///
+		for iTrial = 2:size(cBlockData,1)		
+			cBlockAnaly(iTrial,2) = isequal(cBlockData(iTrial,3),cBlockData(iTrial-1,3));
+			cBlockAnaly(iTrial,3) = isequal(cBlockData(iTrial,4),cBlockData(iTrial-1,4));
+		end				
+		%% ---------------------------- target shape/color end ----------------\\\
+
+
+		%%------- column 4 = same or across hemisphere -------//
+		vertMeridianLoc = [2,6];    
+		leftHemLoc      = [3:5];
+
+
+		for iTrial = 2:size(cBlockData,1)
+
+			if diff(cBlockData([iTrial-1,iTrial],2))==0 
+				cBlockAnaly(iTrial,4) = 100;
+			else
+				if numel(intersect(cBlockData([iTrial-1,iTrial],2),vertMeridianLoc))~=0
+					cBlockAnaly(iTrial,4) = 3; %% vertical meridian 
+				else % nonvertial vertical meridian 
+					cBlockAnaly(iTrial,4) = 1+mod(numel(intersect(cBlockData([iTrial-1,iTrial],2),leftHemLoc)),2); % 1=same, 2=cross
+				end
+			end
+			cBlockAnaly(iTrial,5) = acc(iTrial-1,iBlock);
+		end	
+		%%------------- same or across hemisphere -----------\\
+
+		%%%%%%%%%%%%%%%%%%   trial-by-trial relationship  calculation end %%%%%%%%%%%%%%%%%
+
+
+		cSubAnaly  = [cSubAnaly;cBlockAnaly];
+
+	end %% iBlock
+
+
+
+	cSubData = [repmat([1 str2num(subInfo{1}) str2num(subInfo{2})],size(cSubData,1),1) cSubData]; % 1st column = subGrpBk
+
+	cSubData(cSubData(:,5)==cPair(2,1),9) = 1; %  
+	cSubData(cSubData(:,5)==cPair(2,2),9) = regularGroup-1; % after merging two regularities, always 1 = regular 
+
+	cSubData(:,[10:12]) = [ones(size(cSubData,1),1) acc(:) responseTimes(:)*1000];
+	allSubData          = [allSubData;cSubData];
+	allSubAnaly         = [allSubAnaly;cSubAnaly];
+
+end %% mergeAlldata
+
+allSubData(:,3)         = ceil(allSubData(:,4)/analyBy);
+
+
+%%% distinguish regular trial types 
+regulTrial = find(allSubData(:,9)~=0); % regular trials
+for iRegulTrial = regulTrial'
+	if regularGroup ==2 
+		if randLocGroupNum==2
+			allSubData(iRegulTrial-1,9) = 2; % paired-non-regular 
+		end
+	elseif  regularGroup ==3
+		if randLocGroupNum==2
+			allSubData(iRegulTrial-1,9) = allSubData(iRegulTrial,9)+2;
+		end
+	end
+	allSubData(iRegulTrial,10) = allSubData(iRegulTrial-1,11); % pairedAcc 
+end
+
+
+%%% cutoff outliers 
+rowFilter = ~~allSubData(:,11);
+[gm,gsd,gname,corrgcout] = grpstats(allSubData(rowFilter,12),{allSubData(rowFilter,2),allSubData(rowFilter,4)},{'mean','std','gname','numel'}); %  ,allSubData(rowFilter,9)
+ gname                   = cellfun(@str2double,gname);
+[zscores]                = shiftzs_BCL([corrgcout],0); 
+
+for iRow = 1:size(gname,1)
+
+	conFilter = allSubData(:,2) == gname(iRow,1) & allSubData(:,4) == gname(iRow,2) ; %%% 
+	allSubData(conFilter,13) = gm(iRow) + zscores(iRow)*gsd(iRow);  % 
+	allSubData(conFilter,14) = 200;
+end
+
+allSubData = [allSubData allSubAnaly];
+
+
+
+% %%%--------- report exclusions % in paper--------------/
+rowFilter = ~~allSubData(:,11);
+[gm,gsd,gname,corrgcout] = grpstats(allSubData(rowFilter,12),{allSubData(rowFilter,2)},{'mean','std','gname','numel'});
+mean(corrgcout/512);
+mean(corrgcout/512)-2.5*std(corrgcout/512);
+
+rowFilter = ~~allSubData(:,11) & allSubData(:,12)<allSubData(:,14); 
+[gm,gsem,gname,gcout] = grpstats(allSubData(rowFilter,12),{allSubData(rowFilter,2)},{'mean','sem','gname','numel'});
+
+rowFilter = ~~allSubData(:,11) & allSubData(:,12)>allSubData(:,13);
+[gm,gsem,gname,gcout] = grpstats(allSubData(rowFilter,12),{allSubData(rowFilter,2)},{'mean','sem','gname','numel'});
+mean(gcout/512);
+
+% %%%%%   overall rt  2.5 SD %%%%
+% rowFilter = ~~allSubData(:,11) & allSubData(:,12)>allSubData(:,14) & allSubData(:,12)<=allSubData(:,13); 
+% [gm,gsem,gname,gcout] = grpstats(allSubData(rowFilter,12),{allSubData(rowFilter,2)},{'mean','sem','gname','numel'});
+% mean(gm)+2.5*std(gm);
+
+
+
+%%%%%%%%%%%%%	ACC	 %%%%%%%%%%%%%
+rowFilter =  ~~allSubData(:,11) ;
+[gm,gsem,gname,gcout] = grpstats(allSubData(rowFilter,12),{allSubData(rowFilter,2),allSubData(rowFilter,3),allSubData(rowFilter,9)},{'mean','sem','gname','numel'});
+gname                 = cellfun(@str2double,gname);
+trialNum              = repmat([48 16]', numel(unique(gname(:,1)))*numel(unique(gname(:,2))) ,1);
+ACC                   = [gname,gcout./trialNum];
+accForSpss            = reshape(ACC(:,4),numel(unique(gname(:,2))) * numel(unique(gname(:,3))),numel(unique(gname(:,1))))';
+
+[gm,gsem,gname,gcout] = grpstats(ACC(:,4),{ACC(:,1),ACC(:,3)},{'mean','sem','gname','numel'});
+meanOfBlock			  = reshape(gm,2,34)';
+
+
+
+
+%%%%%%%%%%%%%	RT	%%%%%%%%%%%%%
+rowFilter =  ~~allSubData(:,11)  & allSubData(:,12)>allSubData(:,14) & allSubData(:,12)<=allSubData(:,13);
+
+[gm,gsem,gname,gcout] = grpstats(allSubData(rowFilter,12),{allSubData(rowFilter,2),allSubData(rowFilter,3),allSubData(rowFilter,9)},{'mean','sem','gname','numel'}); % ,allSubData(rowFilter,9)
+gname                 = cellfun(@str2double,gname);
+disRt                 = [gname,gm,gsem,gcout];
+RtForSpss             = reshape(disRt(:,4),16,numel(unique(disRt(:,1))))';
+
+[gm,gsem,gname,gcout] = grpstats(disRt(:,4),{disRt(:,1),disRt(:,3)},{'mean','sem','gname','numel'});
+meanOfBlock			  = reshape(gm,2,34)';
+
+
+cd ..
+
+
+
+catch behAnaly_SL_error
+		cd ..
+		save behAnaly_SL_debug;
+		rethrow(behAnaly_SL_error);		
+end
+
+
+end % function
